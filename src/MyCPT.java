7289323d8c27f5d1248953bdb7d4bf4a16fee856
Dvir <dvirhacohen@gmail.com>
2019-11-27 11:36:31 +0200

    CPT

--- src/MyCPT.java
+++ src/MyCPT.java
@@ -14,7 +14,7 @@ public class MyCPT {
     public static MyCPT build(String[] vars) {
         MyCPT myCPT = new MyCPT();
         String p = vars[2].substring(9);
-        String [] parents = p.split(",");
+        //String [] parents = p.split(",");
 
 
         StringBuilder bs = new StringBuilder();
@@ -25,21 +25,10 @@ public class MyCPT {
         String allData = bs.toString().replaceAll("=true,", "");
         String [] linesData = allData.split("\n");
         for ( String line : linesData){
-            String[] lineData = line.split(",");
-           // StringBuilder stringData = new StringBuilder();
-            String string = "";
-            int parentIndex = 0;
-            for (int data = 0 ; data < lineData.length-1; data++) {
-                string+=parents[parentIndex]+ "="+ lineData[data] +",";
-               // stringData.append(parents[parentIndex]).append("=").append(lineData[data]).append(",");
-                parentIndex++;
-            }
-            MyEntry myEntry = new MyEntry();
-            myEntry.getVarVal().add(string);
-            myEntry.getProbs().add(Double.parseDouble(lineData[lineData.length-1]));
-            myCPT.cpt.add(myEntry);
-
-
+            String data = line;
+            MyEntry entry = new MyEntry();
+            entry.getVarVal().add(data);
+            myCPT.cpt.add(entry);
         }
 
         return myCPT;
@@ -51,6 +40,10 @@ public class MyCPT {
             stringBuilder.append(myEntry.toString()).append("\n");
         }
         return stringBuilder.toString();
+    }
+
+    public int size(){
+        return this.cpt.size();
     }
 }
 
